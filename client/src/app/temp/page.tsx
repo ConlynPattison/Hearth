709e52d11734f7d59806775623caed217542cd1c
ConlynPattison <cpattison@csumb.edu>
2024-10-26 15:07:12 -0700

    Added temp layout for final app design

---
+++ client/src/app/temp/page.tsx
@@ -0,0 +1,29 @@
+const Temp = () => {
+    return (
+        <>
+            <div className="flex h-screen">
+                {/* Left edge nav bar */}
+                <div className="h-[100%] bg-blue-500 w-[80px]">
+                </div>
+
+                {/* Outer container */}
+                <div className="bg-red-600 w-[100%] overflow-y-auto h-[100%] sm:flex">
+
+                    {/* Left middle inbox & rooms */}
+                    <div className="bg-blue-700 sm:w-[400px] h-[100%]">
+                    </div>
+
+                    {/* Right middle messages */}
+                    <div className="bg-slate-500 w-[100%] h-[100%]">
+                    </div>
+
+                    {/* Right edge profile info */}
+                    <div className="bg-violet-400 sm:w-[300px] h-[100%]">
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Temp;
