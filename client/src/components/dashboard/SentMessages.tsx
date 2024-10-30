e629de96491603ec2e038daa3622b1fbfd2c806e
ConlynPattison <cpattison@csumb.edu>
2024-10-30 14:28:26 -0700

    Added timestamps to messages sent

--- client/src/components/dashboard/SentMessages.tsx
+++ client/src/components/dashboard/SentMessages.tsx
@@ -9,7 +9,7 @@ const ProfilePicture = ({ avatarUrl }: { avatarUrl: string }) => {
 			<Image
 				className="rounded-full"
 				alt="PP"
-				src={avatarUrl}
+				src={avatarUrl || ""}
 				width={50}
 				height={50}
 			/>
@@ -21,6 +21,8 @@ const Message = ({ message }: { message: MessageForView }) => {
 	const sharedClasses = "whitespace-pre w-fit flex-wrap text-wrap rounded-xl px-3 py-2 mb-2 mt-1 break-all"
 	const { user, isLoading } = useUser();
 	const userId = user?.sub;
+
+	const localTime = message.time && new Date(message.time).toLocaleTimeString()
 
 	if (!user || isLoading) {
 		return (
@@ -35,14 +37,20 @@ const Message = ({ message }: { message: MessageForView }) => {
 					<div className="flex flex-row-reverse">
 						<div className="flex flex-row-reverse max-w-[85%]">
 							<ProfilePicture avatarUrl={message.avatarUrl} />
-							<p className={`${sharedClasses} bg-gradient-to-tl from-purple-700 to-red-500  rounded-tr-none mr-2`}>{message.content}</p>
+							<div>
+								<p className={`${sharedClasses} bg-gradient-to-tl from-purple-700 to-red-500  rounded-tr-none mr-2`}>{message.content}</p>
+								<p className={`text-xs text-slate-400`}>{localTime}</p>
+							</div>
 						</div>
 					</div>
 					:
 					<div className="flex">
 						<div className="flex max-w-[85%]">
 							<ProfilePicture avatarUrl={message.avatarUrl} />
-							<p className={`${sharedClasses} bg-gradient-to-tl from-red-600 to-yellow-500 rounded-tl-none ml-2`}>{message.content}</p>
+							<div>
+								<p className={`${sharedClasses} bg-gradient-to-tl from-red-600 to-yellow-500 rounded-tl-none ml-2`}>{message.content}</p>
+								<p className={`text-xs text-slate-400 flex flex-row-reverse`}>{localTime}</p>
+							</div>
 						</div>
 					</div>
 			}
