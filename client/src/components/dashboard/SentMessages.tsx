50f520cde54edd967b8a1405c0da08d48127af09
ConlynPattison <cpattison@csumb.edu>
2024-10-30 13:43:32 -0700

    Fixed bug with breaks on continuous character sequences, removed unused objects from props

--- client/src/components/dashboard/SentMessages.tsx
+++ client/src/components/dashboard/SentMessages.tsx
@@ -3,13 +3,13 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 import { MessageForView } from "@chat-app/types";
 import Image from "next/image";
 
-const ProfilePicture = ({ message }: { message: MessageForView }) => {
+const ProfilePicture = ({ avatarUrl }: { avatarUrl: string }) => {
 	return (
 		<div>
 			<Image
 				className="rounded-full"
 				alt="PP"
-				src={message.avatarUrl}
+				src={avatarUrl}
 				width={50}
 				height={50}
 			/>
@@ -18,7 +18,7 @@ const ProfilePicture = ({ message }: { message: MessageForView }) => {
 }
 
 const Message = ({ message }: { message: MessageForView }) => {
-	const sharedClasses = "whitespace-pre w-fit flex-wrap text-wrap rounded-xl px-3 py-2 mb-2 mt-1"
+	const sharedClasses = "whitespace-pre w-fit flex-wrap text-wrap rounded-xl px-3 py-2 mb-2 mt-1 break-all"
 	const { user, isLoading } = useUser();
 	const userId = user?.sub;
 
@@ -34,14 +34,14 @@ const Message = ({ message }: { message: MessageForView }) => {
 				message.userId === userId ?
 					<div className="flex flex-row-reverse">
 						<div className="flex flex-row-reverse max-w-[85%]">
-							<ProfilePicture message={message} />
+							<ProfilePicture avatarUrl={message.avatarUrl} />
 							<p className={`${sharedClasses} bg-gradient-to-tl from-purple-700 to-red-500  rounded-tr-none mr-2`}>{message.content}</p>
 						</div>
 					</div>
 					:
 					<div className="flex">
 						<div className="flex max-w-[85%]">
-							<ProfilePicture message={message} />
+							<ProfilePicture avatarUrl={message.avatarUrl} />
 							<p className={`${sharedClasses} bg-gradient-to-tl from-red-600 to-yellow-500 rounded-tl-none ml-2`}>{message.content}</p>
 						</div>
 					</div>
